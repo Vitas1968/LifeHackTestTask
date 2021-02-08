9d8d71221a3479c3401a13e5b029434a5cbbf823
Vitas1968 <dor1968@mail.ru>
2021-02-08 18:54:58 +0700

    Initial commit

---
+++ app/src/main/java/com/vitaly/lifehacktesttask/ui/MainActivityTestTask.kt
@@ -0,0 +1,12 @@
+package com.vitaly.lifehacktesttask.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.vitaly.lifehacktesttask.R
+
+class MainActivityTestTask : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
